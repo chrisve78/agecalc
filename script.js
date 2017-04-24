97a13b776b663d8e1643f602cf1eace371208041
Chris Ellis <cellis8200@gmail.com>
2017-04-23 18:45:26 -0700

    fix jsfile

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
 var age = prompt("How old are you?");
 
-var leapYearDays = age % 4;
-
-var daysOld = age * 365 + leapYearDays;
+var daysOld = age * 365.25 + leapYearDays;
 
 alert("You are approximately "+ daysOld + " days old.")
